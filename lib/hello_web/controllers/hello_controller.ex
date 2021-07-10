c94d2de27e860fc4446ba617cbf4666a25e62b1f
blue-emc2 <s.yamazaki@everyleaf.com>
2021-07-10 22:52:06 +0900

    いけてないコード

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -5,7 +5,23 @@ defmodule HelloWeb.HelloController do
     render(conn, "index.html")
   end
 
-  def show(conn, %{"messenger" => messenger} = params) do
-    render(conn, "show.html", messenger: messenger)
+  def show(conn, params) do
+    case Authenticator.find_user(conn) do
+      {:ok, user} ->
+        case find_message(params["id"]) do
+          nil ->
+            conn |> put_flash(:info, "That message wasn't found") |> redirect(to: "/")
+
+          message ->
+            if Authorizer.can_access?(user, message) do
+              render(conn, :show, page: message)
+            else
+              conn |> put_flash(:info, "You can't access that page") |> redirect(to: "/")
+            end
+        end
+
+      :error ->
+        conn |> put_flash(:info, "You must be logged in") |> redirect(to: "/")
+    end
   end
 end
